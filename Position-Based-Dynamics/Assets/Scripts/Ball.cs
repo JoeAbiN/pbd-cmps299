85317c157cc6ec1654802346a0d8c4e27ca173a7
Joe Abi Nassif <jfa20@mail.aub.edu>
2020-03-16 18:14:04 +0200

    Integrated nvidia flex and made ball interactive

---
+++ Position-Based-Dynamics/Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    private float force = 500f;
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            // rigidbody.AddForce((Vector3.forward + Vector3.up) * force);
+            // Vector3 tempVelocity = rigidbody.velocity;
+            // rigidbody.velocity = new Vector3(tempVelocity.x, tempVelocity.y * force/2, tempVelocity.z * force);
+            rigidbody.AddForce(Vector3.forward * force + Vector3.up * force/2);
+        }
+    }
+}
---
+++ Position-Based-Dynamics/Assets/Scripts/FlexCloth.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NVIDIA.Flex;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class FlexCloth : MonoBehaviour {
+    public FlexClothActor actor;
+    private FlexClothAsset asset;
+    private Mesh refMesh;
+
+    private MeshFilter meshFilter;
+    private Mesh myMesh;
+    
+    void Start() {
+        asset = actor.asset;
+        refMesh = asset.referenceMesh;
+        
+        meshFilter = GetComponent<MeshFilter>();
+        myMesh = meshFilter.mesh;
+
+        Render();
+    }
+
+    void Update() {
+        // for (int i = 0; i < myMesh.vertices.Length; i++) {
+        //     Vector3 tempPos = myMesh.vertices[i].position;
+        //     myMesh.vertices[i].position = new Vector3(tempPos.x, tempPos.y, (float)Math.Sin(Time.time - tempPos.x));
+        // }
+        
+        Render();
+    }
+
+    void Render() {
+        myMesh.Clear();
+        myMesh.vertices = refMesh.vertices;
+        for (int i = 0; i < myMesh.vertices.Length; i++) {
+            Vector3 tempPos = myMesh.vertices[i];
+            myMesh.vertices[i] = new Vector3(tempPos.x, tempPos.y, (float)Math.Sin(Time.time - tempPos.x));
+        }
+        myMesh.triangles = refMesh.triangles;
+        myMesh.RecalculateNormals();
+    }
+}
