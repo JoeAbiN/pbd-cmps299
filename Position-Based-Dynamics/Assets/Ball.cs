fe66d28bae9ba43b14dca3e3a7b03d65890f06ab
Joe Abi Nassif <jfa20@mail.aub.edu>
2020-04-22 23:44:32 +0300

    updates

---
+++ Position-Based-Dynamics/Assets/Ball.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    private Vector3 origiPos;
+    private Rigidbody rigidbody;
+    public Transform camera;
+    public GameObject camHandler;
+    private Camera camComp;
+    
+    public float force = 0;
+    public float maxForce = 1000;
+    private Vector3 F;
+    float y;
+    float dt;
+    
+    public float n = 5f;
+
+    public GameObject shotBar;
+    private RectTransform barTransform;
+
+    private LineRenderer lineRenderer;
+
+    void Start() {
+        origiPos = transform.position;
+        rigidbody = GetComponent<Rigidbody>();
+        camComp = camHandler.GetComponent<CamSwitch>().targetCam0.GetComponent<Camera>();
+        
+        dt = Time.fixedDeltaTime;
+        y = 0.5f;
+        force = 0;
+        
+        barTransform = shotBar.GetComponent<RectTransform>();
+        barTransform.sizeDelta = new Vector2(0, barTransform.sizeDelta.y);
+
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update() {
+        F = new Vector3(camera.forward.x, y, camera.forward.z);
+
+        if (camComp.depth == 0) {
+            lineRenderer.SetPosition(1, origiPos + 0*F);
+        
+        } else {
+            lineRenderer.SetPosition(1, origiPos + 8*F);
+        }
+        lineRenderer.SetPosition(0, origiPos);
+        
+        if (Input.GetKey(KeyCode.W)) {
+            y += 0.1f * dt;
+        
+        } else if (Input.GetKey(KeyCode.S)) {
+            y -= 0.1f * dt;
+        }
+
+        if (Input.GetKey(KeyCode.Space)) {
+            if (force < maxForce) {
+                force += 10;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            barTransform.sizeDelta = new Vector2(0, barTransform.sizeDelta.y);
+            rigidbody.velocity = F * force * dt;
+            force = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            force = 0;
+            rigidbody.velocity = Vector3.zero;
+            transform.position = origiPos;
+        }
+
+        barTransform.sizeDelta = new Vector2(force / n, barTransform.sizeDelta.y);
+    }
+}
---
+++ Position-Based-Dynamics/Assets/CamSwitch.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject targetCam0;
+    public GameObject targetCam1;
+    public GameObject targetCam2;
+    public GameObject targetCam3;
+    public GameObject targetCam4;
+    public GameObject targetCam5;
+	
+	private Camera cam0;
+    private Camera cam1;
+    private Camera cam2;
+    private Camera cam3;
+    private Camera cam4;
+    private Camera cam5;
+
+	void Start()
+	{
+		cam0 = targetCam0.GetComponent<Camera>();
+		cam1 = targetCam1.GetComponent<Camera>();
+		cam2 = targetCam2.GetComponent<Camera>();
+		cam3 = targetCam3.GetComponent<Camera>();
+		cam4 = targetCam4.GetComponent<Camera>();
+		cam5 = targetCam5.GetComponent<Camera>();
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            cam0.depth = 1;
+            cam1.depth = 0;
+            cam2.depth = 0;
+            cam3.depth = 0;
+            cam4.depth = 0;
+            cam5.depth = 0;
+        }
+		
+		if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            cam0.depth = 0;
+            cam1.depth = 1;
+            cam2.depth = 0;
+            cam3.depth = 0;
+            cam4.depth = 0;
+            cam5.depth = 0;
+        }
+		
+		if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            cam0.depth = 0;
+            cam1.depth = 0;
+            cam2.depth = 1;
+            cam3.depth = 0;
+            cam4.depth = 0;
+            cam5.depth = 0;
+        }
+		
+		if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            cam0.depth = 0;
+            cam1.depth = 0;
+            cam2.depth = 0;
+            cam3.depth = 1;
+            cam4.depth = 0;
+            cam5.depth = 0;
+        }
+		
+		if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            cam0.depth = 0;
+            cam1.depth = 0;
+            cam2.depth = 0;
+            cam3.depth = 0;
+            cam4.depth = 1;
+            cam5.depth = 0;
+        }
+		
+		if (Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            cam0.depth = 0;
+            cam1.depth = 0;
+            cam2.depth = 0;
+            cam3.depth = 0;
+            cam4.depth = 0;
+            cam5.depth = 1;
+        }
+    }
+}
